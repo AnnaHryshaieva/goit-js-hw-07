60cc920fb3755bf1440a9e3431f371a76d5ae732
AnnaHryshaieva <anet01073@gmail.com>
2024-01-24 00:35:40 +0200

    changed

--- js/task-3.js
+++ js/task-3.js
@@ -2,8 +2,8 @@ const nameInput = document.querySelector('input#name-input');
 const nameOutput = document.querySelector('span#name-output');
 
 nameInput.addEventListener('input', event => {
-  nameOutput.textContent = event.currentTarget.value.trim();
-  if (event.currentTarget.value === '') {
+  nameOutput.textContent = event.target.value.trim();
+  if (event.target.value === '') {
     nameOutput.textContent = 'Anonymous';
   }
 });
